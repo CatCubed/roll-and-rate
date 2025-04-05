4388e929e0e24f30f0d09c87d2890bee7a800686
Ladislav Kořínek <LadaKorinek@gmail.com>
2025-04-05 17:59:53 +0200

    ftr: footer: implement

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,7 @@
-<?php
+<footer class="bg-dark footer">
+    <p class="footer__item">&copy;<?= date('Y') ?> Studentský projekt studentů <a class="footer__item__link" href="https://www.pef.czu.cz/cs">ČZU - PEF</a></p>
+</footer>
+
+<script src="assets/js/dynamicStyling.js"></script>
+</body>
+</html>
