a46000fcb8028a03bee4fd946ec6ecdceb980df7
Ladislav Kořínek <LadaKorinek@gmail.com>
2025-04-05 18:00:45 +0200

    ftr: header: implement

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/styles-mobile.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/icon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href='https://fonts.googleapis.com/css?family=Jaini' rel='stylesheet'>
+    <title>Roll & Rate | <?= $pageTitle ?? 'Deskovky' ?></title>
+</head>
+<body>
