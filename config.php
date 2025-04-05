9bc372c1cba4833145639deef2da588bee688a32
Ladislav Kořínek <LadaKorinek@gmail.com>
2025-04-05 18:02:29 +0200

    ftr: config: implement

--- config.php
+++ config.php
@@ -1,1 +1,17 @@
 <?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');  // TODO change
+define('DB_PASS', '');      // TODO change
+define('DB_NAME', 'rollandrate');
+
+define('BASE_URL', 'http://localhost/roll-and-rate');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function redirect($page) {
+    header("Location: " . BASE_URL . "?page=" . $page);
+    exit;
+}
+?>
