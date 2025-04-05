609ab21758f91bc4335031332b6f98836fe83322
Ladislav Kořínek <LadaKorinek@gmail.com>
2025-04-05 18:00:50 +0200

    ftr: navbar: implement

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,1 +1,34 @@
-<?php
+<!-- NAVBAR - Desktop -->
+<nav class="display-large bg-dark text-white navbar">
+    <img width="191" height="40" class="navbar__logo" src="assets/images/logo.png" alt="logo">
+    <a class="navbar__item bg-primary <?= $page == 'home' ? 'navbar__item--selected' : '' ?>" href="index.php?page=home">
+        <i class="navbar__item__icon material-icons">home</i>Domů
+    </a>
+    <a class="navbar__item bg-primary <?= $page == 'reviews' ? 'navbar__item--selected' : '' ?>" href="index.php?page=reviews">
+        <i class="navbar__item__icon material-icons">star</i>Recenze
+    </a>
+    <a class="navbar__item bg-primary <?= $page == 'browse' ? 'navbar__item--selected' : '' ?>" href="index.php?page=browse">
+        <i class="navbar__item__icon material-icons">web</i>Procházet
+    </a>
+    <a class="navbar__item bg-primary <?= $page == 'wishlist' ? 'navbar__item--selected' : '' ?>" href="index.php?page=wishlist">
+        <i class="navbar__item__icon material-icons">favorite</i>Oblíbené
+    </a>
+</nav>
+<!-- NAVBAR - Desktop -->
+
+<!-- NAVBAR - Mobile -->
+<nav class="display-small bg-dark navbar">
+    <a class="navbar__item bg-primary <?= $page == 'home' ? 'navbar__item--selected' : '' ?>" href="index.php?page=home">
+        <i class="navbar__item__icon material-icons">home</i>
+    </a>
+    <a class="navbar__item bg-primary <?= $page == 'reviews' ? 'navbar__item--selected' : '' ?>" href="index.php?page=reviews">
+        <i class="navbar__item__icon material-icons">star</i>
+    </a>
+    <a class="navbar__item bg-primary <?= $page == 'browse' ? 'navbar__item--selected' : '' ?>" href="index.php?page=browse">
+        <i class="navbar__item__icon material-icons">web</i>
+    </a>
+    <a class="navbar__item bg-primary <?= $page == 'wishlist' ? 'navbar__item--selected' : '' ?>" href="index.php?page=wishlist">
+        <i class="navbar__item__icon material-icons">favorite</i>
+    </a>
+</nav>
+<!-- NAVBAR - Mobile -->
