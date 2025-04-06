3f821b7296463421698cd89c3525e35c3f234292
Ladislav Kořínek <LadaKorinek@gmail.com>
2025-04-06 10:28:26 +0200

    fix: database.sql: use new connection

--- database.sql
+++ database.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE IF NOT EXISTS rollandrate CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-USE rollandrate;
+USE ete32e_2425zs_13;
 
 CREATE TABLE IF NOT EXISTS games (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
+                                     id INT AUTO_INCREMENT PRIMARY KEY,
+                                     title VARCHAR(255) NOT NULL UNIQUE,
     description TEXT NOT NULL,
     rules TEXT NOT NULL,
     image VARCHAR(255) NOT NULL,
@@ -14,23 +12,23 @@ CREATE TABLE IF NOT EXISTS games (
     genre VARCHAR(100) NOT NULL,
     playerCount INT NOT NULL,
     reviewScore DECIMAL(3,1) NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS game_images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    game_id INT NOT NULL,
-    image VARCHAR(255) NOT NULL,
+                                           id INT AUTO_INCREMENT PRIMARY KEY,
+                                           game_id INT NOT NULL,
+                                           image VARCHAR(255) NOT NULL,
     FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
-);
+    );
 
 CREATE TABLE IF NOT EXISTS reviews (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
+                                       id INT AUTO_INCREMENT PRIMARY KEY,
+                                       title VARCHAR(255) NOT NULL UNIQUE,
     text TEXT NOT NULL,
     image VARCHAR(255) NOT NULL
-);
+    );
 
--- TODO use better data
+-- TODO update data
 INSERT INTO games (title, description, rules, image, favorite, releaseYear, difficulty, genre, playerCount, reviewScore)
 VALUES
     ('Test 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', 'Lorem ipsum dolor sit amet...', 'icon.ico', 0, 2020, 1, 'Genre 1', 4, 7.5),
@@ -39,7 +37,6 @@ VALUES
     ('Test 4', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', 'Lorem ipsum dolor sit amet...', 'icon.ico', 0, 2022, 1, 'Genre 2', 2, 7.0),
     ('Test 5', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', 'Lorem ipsum dolor sit amet...', 'icon.ico', 1, 2024, 2, 'Genre 1', 4, 6.0);
 
--- Vložení testovacích dat - recenze
 INSERT INTO reviews (title, text, image)
 VALUES
     ('Test 1 Review', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', 'icon.ico'),
